191c5af39104b2f0dab3a35f1c51e2b43668e650
arioliveirari <oliveiraariel96@gmail.com>
2023-01-31 04:27:03 -0300

    function to change color for horse done
    two bugs remain, limits of the board and resetColor when new button is clicked.

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 
 
-function test(){
-    console.log("funca")
-    let backgroundChess = document.getElementById("1")
-    backgroundChess.style.backgroundColor = "red"
+
+function resetColors(){
+    
 }
+
+
+
+
+
+function test(){
+    resetColors()
+    let row = arguments[0]
+    let col = arguments[1]
+    console.log(row)
+    console.log(col)
+
+    let right2Up1   = String(row + 1) + String(col + 2)
+    let right2Down1 = String(row - 1) + String(col + 2)
+    let right1Up2   = String(row + 2) + String(col + 1) 
+    let right1Down2 = String(row - 2) + String(col + 1)
+    let left1Down2  = String(row - 2) + String(col - 1)
+    let left1Up2    = String(row + 2) + String(col - 1)
+    let left2Up1    = String(row + 1) + String(col - 2)
+    let left2Down1  = String(row - 1) + String(col - 2)
+    
+    let possibleSquares = [
+        right2Up1       ,
+        right2Down1     ,
+        right1Up2       ,
+        right1Down2     ,
+        left1Down2      ,
+        left1Up2        ,
+        left2Up1        ,
+        left2Down1      ,
+    ]
+    
+    for (let i = 0 ; 1  < 9 ; i++){
+        console.log(i)
+        console.log(possibleSquares[i])
+        document.getElementById(possibleSquares[i]).style.backgroundColor = "green"
+    }
+    
+}
+
+
+
